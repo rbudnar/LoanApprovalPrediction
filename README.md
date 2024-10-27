adc7f7aa4ae4bd7a4b89acd17d1d6ffa9937743b
Ron Budnar <budnarr@gmail.com>
2024-10-27 11:58:20 -0500

    Add basic XGBoost model

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Loan Approval Prediction
 
 ### Playground Series - Season 4, Episode 10
+[Competition homepage](https://www.kaggle.com/competitions/playground-series-s4e10/data): 
---
+++ loanapprovalprediction/constants.py
@@ -0,0 +1,15 @@
+TARGET = "loan_status"
+CONTINUOUS_VARIABLES = [
+    "person_age",
+    "person_income",
+    "loan_amnt",
+    "loan_int_rate",
+    "loan_percent_income",
+    "cb_person_cred_hist_length",
+]
+CATEGORICAL_VARIABLES = [
+    "person_home_ownership",
+    "loan_intent",
+    "loan_grade",
+    "cb_person_default_on_file",
+]
